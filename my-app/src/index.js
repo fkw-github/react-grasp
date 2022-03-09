0400b4999579aec83a11c7d89f28f7cb28c173cd
lanmeidada <lanmeidada@qq.com>
2022-03-09 17:53:26 +0800

    实现判断出胜利的一方

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-/* 这是类组件 */
+// 这是类组件
 // class Square extends React.Component {
 //   render() {
 //     return (
@@ -16,7 +16,7 @@ import './index.css';
 //   }
 // }
 
-/* 这是函数组件 */
+// 这是函数组件
 function Square(props) {
   return (
     <button className='square' onClick={props.onClick}>
@@ -53,7 +53,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'winner:' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -100,3 +106,24 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+// 判断胜者
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for(let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
